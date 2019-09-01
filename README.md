f2ced6bff1e932d8c3d02faadc13279e7da1931e
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-09-01 13:37:03 +0200

    Added travis to project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# fps-project
+# fps-project [![Build Status](https://travis-ci.org/Alberto-Izquierdo/fps-project.svg?branch=master)](https://travis-ci.org/Alberto-Izquierdo/fps-project)
--- setup_environment.sh
+++ setup_environment.sh
@@ -1,1 +1,2 @@
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add anotherfoxguy https://api.bintray.com/conan/anotherfoxguy/ror-dependencies
