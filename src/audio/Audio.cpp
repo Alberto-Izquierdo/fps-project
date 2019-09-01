b2af6c178e9af6b2239e9d0fc2e36a876c8466e1
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-09-01 10:17:00 +0200

    Concrete RAII structs replaced with generic class container

--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -1,124 +1,115 @@
 #include "Audio.h"
 #include <libavcodec/avcodec.h>
 #include <libavutil/mathematics.h>
+#include <filesystem>
 
 #define AUDIO_INBUF_SIZE 20480
 #define AUDIO_REFILL_THRESH 4096
 
 using namespace aiaudio;
 
-struct AVPacketRAII {
-    AVPacketRAII()
-        : pkt(av_packet_alloc())
+template <typename T, typename F>
+class RAIIContainer
+{
+public:
+    RAIIContainer(T *value, F releaseFunction)
+        : m_value(value)
+        , m_releaseFunction(releaseFunction)
     {
     }
-    ~AVPacketRAII() { av_packet_free(&pkt); }
-    AVPacket *operator->() const { return pkt; }
-    bool operator!() const { return pkt != nullptr; }
-
-private:
-    AVPacket *pkt = nullptr;
-};
-
-struct AVCodecParserContextRAII {
-    AVCodecParserContextRAII(AVCodecID codecId)
-        : parser(av_parser_init(codecId))
+    ~RAIIContainer()
     {
-    }
-    ~AVCodecParserContextRAII() { av_parser_close(parser); }
-    AVCodecParserContext &operator->() const { return *parser; }
-    bool operator!() const { return parser != nullptr; }
-    AVCodecParserContext *getPtr() const { return parser; }
-
-private:
-    AVCodecParserContext *parser = nullptr;
-};
-
-struct AVFrameRAII {
-    AVFrameRAII() {}
-    ~AVFrameRAII()
-    {
-        if (frame) {
-            av_frame_free(&frame);
+        if (m_value != nullptr) {
+            m_releaseFunction();
+            m_value = nullptr;
         }
     }
-    AVFrame &operator->() const { return *frame; }
-    bool operator!() const { return frame != nullptr; }
-    AVFrame *&getPtr() { return frame; }
+    T *operator->() const { return m_value; }
+    bool operator!() const { return m_value != nullptr; }
+    T *get() const { return m_value; }
+    T *&get() { return m_value; }
 
 private:
-    AVFrame *frame = nullptr;
+    T *m_value;
+    F m_releaseFunction;
 };
 
-struct FileRAII {
-    FileRAII(const char *filepath, const char *mode)
-        : f(fopen(filepath, mode))
-    {
+static std::optional<std::string> decode(AVCodecContext *dec_ctx, AVPacket *pkt, AVFrame *frame, FILE *outfile)
+{
+    int i, ch;
+    int ret, data_size;
+    /* send the packet with the compressed data to the decoder */
+    ret = avcodec_send_packet(dec_ctx, pkt);
+    if (ret < 0) {
+        return "Error submitting the packet to the decoder";
     }
-    ~FileRAII() { fclose(f); }
-    FILE &operator->() const { return *f; }
-    bool operator!() const { return f != nullptr; }
-    FILE *getPtr() const { return f; }
-
-private:
-    FILE *f = nullptr;
-};
-
-struct AVCodecContextRAII {
-    AVCodecContextRAII(const AVCodec *codec)
-        : c(avcodec_alloc_context3(codec))
-    {
+    /* read all the output frames (in general there may be any number of them */
+    while (ret >= 0) {
+        ret = avcodec_receive_frame(dec_ctx, frame);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+            return {};
+        else if (ret < 0) {
+            return "Error during decoding";
+        }
+        data_size = av_get_bytes_per_sample(dec_ctx->sample_fmt);
+        if (data_size < 0) {
+            /* This should not occur, checking just for paranoia */
+            return "Failed to calculate data size";
+        }
+        for (i = 0; i < frame->nb_samples; i++) {
+            for (ch = 0; ch < dec_ctx->channels; ch++) {
+                fwrite(frame->data[ch] + data_size * i, 1, data_size, outfile);
+            }
+        }
     }
-    ~AVCodecContextRAII() { avcodec_free_context(&c); }
-    AVCodecContext &operator->() const { return *c; }
-    bool operator!() const { return c != nullptr; }
-    AVCodecContext *getPtr() const { return c; }
-
-private:
-    AVCodecContext *c = nullptr;
-};
+}
 
 std::variant<std::vector<float>, std::string> Audio::getVolumeValuesFromAudioFile(const char *path)
 {
-    AVPacketRAII pkt;
+    AVPacket *pktPtr = av_packet_alloc();
+    RAIIContainer pkt(pktPtr, [&]() { av_packet_free(&pktPtr); });
 
     const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_MP3);
     if (!codec) {
         return std::string("Codec not found");
     }
 
-    AVCodecParserContextRAII parser(codec->id);
+    AVCodecParserContext *parserPtr;
+    RAIIContainer parser(parserPtr, [&]() { av_parser_close(parserPtr); });
     if (!parser) {
         return std::string("Parser not found");
     }
 
-    AVCodecContextRAII c(codec);
+    AVCodecContext *cPtr = avcodec_alloc_context3(codec);
+    RAIIContainer c(cPtr, [&]() { avcodec_free_context(&cPtr); });
     if (!c) {
         return std::string("Could not allocate context");
     }
 
-    if (avcodec_open2(c.getPtr(), codec, nullptr) < 0) {
+    if (avcodec_open2(c.get(), codec, nullptr) < 0) {
         return std::string("Could not open codec");
     }
 
-    FileRAII f(path, "rb");
+    FILE *fPtr = fopen(path, "rb");
+    RAIIContainer f(fPtr, [&]() { fclose(fPtr); });
     if (!f) {
         return std::string("Error opening file %s", path);
     }
 
     uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
     uint8_t *data = inbuf;
-    size_t data_size = fread(inbuf, 1, AUDIO_INBUF_SIZE, f.getPtr());
-    AVFrameRAII decoded_frame;
+    size_t data_size = fread(inbuf, 1, AUDIO_INBUF_SIZE, f.get());
+    AVFrame *decodedFramePtr = nullptr;
+    RAIIContainer decoded_frame(decodedFramePtr, [&]() { av_frame_free(&decodedFramePtr); });
 
     while (data_size > 0) {
         if (!decoded_frame) {
-            if (!(decoded_frame.getPtr() = av_frame_alloc())) {
+            if (!(decoded_frame.get() = av_frame_alloc())) {
                 return std::string("Could not allocate audio frame");
             }
         }
         int ret = av_parser_parse2(
-            parser.getPtr(), c.getPtr(), &pkt->data, &pkt->size, data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+            parser.get(), c.get(), &pkt->data, &pkt->size, data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
         if (ret < 0) {
             return std::string("Error while parsing");
         }
@@ -127,12 +118,12 @@ std::variant<std::vector<float>, std::string> Audio::getVolumeValuesFromAudioFil
         data_size -= ret;
 
         if (pkt->size) {
-            // decode(c, pkt->pkt, decoded_frame, outfile);
+            //auto result = decode(c.get(), pkt.get(), decoded_frame.get(), outfile);
         }
         if (data_size < AUDIO_REFILL_THRESH) {
             memmove(inbuf, data, data_size);
             data = inbuf;
-            int len = fread(data + data_size, 1, AUDIO_INBUF_SIZE - data_size, f.getPtr());
+            int len = fread(data + data_size, 1, AUDIO_INBUF_SIZE - data_size, f.get());
             if (len > 0)
                 data_size += len;
         }
